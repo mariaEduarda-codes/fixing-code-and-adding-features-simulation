7a5aac9038849af23c3c84944e752b8d41c0b4bc
madu <medeiroslima.me@gmail.com>
2024-10-23 23:06:34 -0300

    feat: added new endpoints for updating title and description

--- models/task.js
+++ models/task.js
@@ -2,12 +2,34 @@ import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
+    description: String,
     status: { type: String, default: 'to-do' },
     dueDate: { type: Date, required: true }
 });
 
 taskSchema.methods.markAsCompleted = function () {
     this.status = 'completed';
+    return this.save();
+};
+
+taskSchema.methods.markAsInProgress = function () {
+    this.status = 'in-progress';
+    return this.save();
+};
+
+taskSchema.methods.setNewTitle = function (title) {
+    this.title = title;
+    return this.save();
+};
+
+taskSchema.methods.setNewDescription = function (description) {
+    this.description = description;
+    return this.save();
+};
+
+taskSchema.methods.setNewTitleAndDescription = function (title, description) {
+    this.title = title;
+    this.description = description;
     return this.save();
 };
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Criação de uma nova tarefa
 router.post('/tasks', async (req, res) => {
-    const { title, dueDate } = req.body;
+    const { title, dueDate, description } = req.body;
     if (!title || !dueDate) {
         return res.status(400).json({ error: 'Title and dueDate are required' });
     }
@@ -17,7 +17,7 @@ router.post('/tasks', async (req, res) => {
     }
 
     try {
-        const task = new Task({ title, dueDate });
+        const task = new Task({ title, dueDate, description });
         await task.save();
         res.status(201).json(task);
     } catch (error) {
@@ -44,6 +44,72 @@ router.patch('/tasks/:id/complete', async (req, res) => {
         }
         await task.markAsCompleted();
         res.status(200).json({ message: 'Task marked as completed' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update task' });
+    }
+});
+
+router.patch('/tasks/:id/in-progress', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+        await task.markAsInProgress();
+        res.status(200).json({ message: 'Task marked as in-progress' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update task' });
+    }
+});
+
+router.patch('/tasks/:id/editTitle', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        const newTitle = req.body.title;
+
+        await task.setNewTitle(newTitle);
+        res.status(200).json({ message: 'Task title has been renamed.' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update task' });
+    }
+});
+
+router.patch('/tasks/:id/editDescription', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        const newDescription = req.body.description;
+
+        await task.setNewDescription(newDescription);
+        res.status(200).json({ message: 'Task description has been renamed.' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update task' });
+    }
+});
+
+router.patch('/tasks/:id/editTitleAndDescription', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        const newTitle = req.body.title;
+        const newDescription = req.body.description;
+
+        if (!newTitle && !newDescription) {
+            return res.status(400).json({ error: 'At least one of title or description must be provided.'});
+        }
+
+        await task.setNewTitleAndDescription(newTitle, newDescription);
+        res.status(200).json({ message: 'Task title has been renamed.' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to update task' });
     }
